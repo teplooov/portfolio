7e39fb2c1b81c658ba1ad1c822b77fe1fdbf48cb
Alex <tepsasha@mail.ru>
2019-06-14 16:44:29 +0300

    bootstrap
    candy

--- WebDeveloper/js/script.js
+++
@@ -1,9 +0,0 @@
- menu.onclick = function myFunction(){
-	var x = document.getElementById('mytopnav');
-
-	if(x.className === "top__nav"){
-		x.className += " responsive";
-	} else {
-		x.className = "top__nav";
-	}
-}
